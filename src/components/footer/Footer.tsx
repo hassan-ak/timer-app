2b241a2e8e102b062a44324679cf9727804417d9
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-11 05:50:55 +0500

    Apply basic styling to app

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
     title: {
-        color: 'black'
+        color: 'white'
     },
 }));
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,7 @@
+// Imports
+// React Imports
 import React from 'react';
+// Material UI Imports
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,33 +9,37 @@ import Typography from '@material-ui/core/Typography';
 import TimerIcon from '@material-ui/icons/Timer';
 import { IconButton } from '@material-ui/core';
 
+// Styling for Header Component
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(0),
-  },
-  title: {
-    flexGrow: 1,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(0),
+    },
+    title: {
+        flexGrow: 1,
+    },
 }));
 
+// Header component
 export default function Header() {
-  const classes = useStyles();
+    // use Styles const
+    const classes = useStyles();
+    // component return
+    return (
+        <div className={classes.root}>
+            <AppBar position="fixed" style={{ background: 'black' }}>
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="fixed" style={{ background: 'black' }}>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="timer">
-            <TimerIcon style={{ fontSize: 40 }}/>
-          </IconButton>
-          <Typography variant="h5" className={classes.title}>
-            Timer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="timer">
+                        <TimerIcon style={{ fontSize: 40 }}/>
+                    </IconButton>
+                    <Typography variant="h5" className={classes.title}>
+                        Timer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
